a72412886698246a38ec4eec7db0f59508415696
atty31 <naghi.attila@gmail.com>
2019-09-17 19:52:11 +0200

    Created module structure.

---
+++ Block/Subscription.php
@@ -0,0 +1,10 @@
+<?php
+namespace Atty31\Subscription\Block;
+
+class Subscription extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Atty31_Subscription',
+    __DIR__
+);
