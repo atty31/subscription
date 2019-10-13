731695e73b063228a3b8cea1e25ab475c111ee16
atty31 <naghi.attila@gmail.com>
2019-10-13 12:20:48 +0200

    Added back model.

--- Model/ResourceModel/Subscription/Collection.php
+++ Model/ResourceModel/Subscription/Collection.php
@@ -1,1 +1,21 @@
-Subscription
+<?php
+namespace Atty31\Subscription\Model\ResourceModel\Subscription;
+
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    protected $_idFieldName = 'id';
+    protected $_eventPrefix = 'atty31_subscription_collection';
+    protected $_eventObject = 'subscription_collection';
+
+    /**
+     * Define resource model
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init('Atty31\Subscription\Model\Subscription', 'Atty31\Subscription\Model\ResourceModel\Subscription');
+    }
+
+}
+
