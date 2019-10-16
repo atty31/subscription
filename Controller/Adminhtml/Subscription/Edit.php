d701a85b761fc25648c23d92d83984d3f3e834f5
atty31 <naghi.attila@gmail.com>
2019-10-16 19:48:45 +0200

    Created subscription form.

--- Controller/Adminhtml/Subscription/Edit.php
+++ Controller/Adminhtml/Subscription/Edit.php
@@ -1,22 +1,80 @@
 <?php
 namespace Atty31\Subscription\Controller\Adminhtml\Subscription;
 
+use Magento\Backend\App\Action;
+
 class Edit extends \Magento\Backend\App\Action
 {
-    protected $resultPageFactory = false;
-
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+    /**
+     * @param Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Framework\Registry $registry
+     */
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
-    )
-    {
-        parent::__construct($context);
+        Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Framework\Registry $registry
+    ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->_coreRegistry = $registry;
+        parent::__construct($context);
     }
-
+    /**
+     * Init actions
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    protected function _initAction()
+    {
+        // load layout, set active menu and breadcrumbs
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        return $resultPage;
+    }
+    /**
+     * Edit Subscription
+     *
+     * @return \Magento\Backend\Model\View\Result\Page|\Magento\Backend\Model\View\Result\Redirect
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
     public function execute()
     {
-        //@todo
+        $id = $this->getRequest()->getParam('id');
+        $model = $this->_objectManager->create('Atty31\Subscription\Model\Subscription');
+        if ($id) {
+            $model->load($id);
+            if (!$model->getId()) {
+                $this->messageManager->addError(__('This subscription no longer exists.'));
+                /** \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+                $resultRedirect = $this->resultRedirectFactory->create();
+                return $resultRedirect->setPath('*/*/');
+            }
+        }
+        $data = $this->_objectManager->get('Magento\Backend\Model\Session')->getFormData(true);
+        if (!empty($data)) {
+            $model->setData($data);
+        }
+        $this->_coreRegistry->register('subscription', $model);
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->_initAction();
+        $resultPage->addBreadcrumb(
+            $id ? __('Edit Subscription') : __('New Subscription'),
+            $id ? __('Edit Subscription') : __('New Subscription')
+        );
+        $resultPage->getConfig()->getTitle()->prepend(__('Subscriptions'));
+        $resultPage->getConfig()->getTitle()
+            ->prepend($model->getId() ? $model->getTitle() : __('New Subscription'));
+        return $resultPage;
     }
 
 }
