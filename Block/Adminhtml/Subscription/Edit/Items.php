077cdf95be6880383a8b588f5ca58f36d4226fba
atty31 <naghi.attila@gmail.com>
2019-10-28 19:50:02 +0100

    Display customer info on the subscription view page.

---
+++ Block/Adminhtml/Subscription/Edit/Items.php
@@ -0,0 +1,15 @@
+<?php
+namespace Atty31\Subscription\Block\Adminhtml\Subscription\Edit;
+
+class Items extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    {
+        parent::__construct($context);
+    }
+
+    public function sayHello()
+    {
+        return __('Hello World');
+    }
+}
