4b83485b6269d8a9dc4d716c5e546862db936db9
atty31 <naghi.attila@gmail.com>
2019-12-25 11:46:17 +0100

    Display subscription items.

--- Block/Adminhtml/Subscription/Edit/Items.php
+++ Block/Adminhtml/Subscription/Edit/Items.php
@@ -1,15 +1,31 @@
 <?php
 namespace Atty31\Subscription\Block\Adminhtml\Subscription\Edit;
 
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Framework\ObjectManagerInterface;
+
 class Items extends \Magento\Framework\View\Element\Template
 {
-    public function __construct(\Magento\Framework\View\Element\Template\Context $context)
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ObjectManagerInterface $objectManager
+    )
     {
+        $this->coreRegistry = $registry;
+        $this->objectManager = $objectManager;
         parent::__construct($context);
     }
 
-    public function sayHello()
+    /**
+     * @return mixed
+     */
+    public function getSubscriptionItemInformation()
     {
-        return __('Hello World');
+        /** @var \Atty31\Subscription\Model\subscription $model */
+        $model = $this->coreRegistry->registry('subscription');
+
+        return $this->objectManager->create('Atty31\Subscription\Model\Items')->load($model->getData('id'), 'subscription_id');
     }
 }
