9f69434445b8590eea3d840ad17c351bf88f8b1d
atty31 <naghi.attila@gmail.com>
2019-12-31 15:17:07 +0100

    Filter product grid after number of days in the subscription view page.

--- Ui/DataProvider/Product/ProductDataProvider.php
+++ Ui/DataProvider/Product/ProductDataProvider.php
@@ -2,6 +2,7 @@
 namespace Atty31\Subscription\Ui\DataProvider\Product;
 
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\Registry;
 
 class ProductDataProvider extends \Magento\Catalog\Ui\DataProvider\Product\ProductDataProvider
 {
@@ -22,13 +23,17 @@ class ProductDataProvider extends \Magento\Catalog\Ui\DataProvider\Product\Produ
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $collectionFactory,
+        Registry $registry,
         array $addFieldStrategies = [],
         array $addFilterStrategies = [],
         array $meta = [],
         array $data = []
     ) {
         parent::__construct($name, $primaryFieldName, $requestFieldName, $collectionFactory, $addFieldStrategies, $addFilterStrategies, $meta, $data);
+
+        $model = $registry->registry('subscription');
         $this->collection->addAttributeToFilter('subscription', '1');
+        $this->collection->addAttributeToFilter('number_of_days', $model->getData('number_of_days'));
     }
 
 }
